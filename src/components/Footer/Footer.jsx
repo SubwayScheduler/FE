871066d7b93ed3868ea2678449bdda5736463419
kwdahun <kwdahun@naver.com>
2024-11-29 16:05:53 +0900

    Design: Footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="py-10 bg-black text-white">
+      <div className="md:w-9/12 w-11/12 mx-auto text-sm pb-5">
+        <div>사이트 제작자</div>
+        <div>동국대학교 컴퓨터공학과 권다훈</div>
+        <div>동국대학교 컴퓨터공학과 류성호</div>
+      </div>
+      <div className="md:w-9/12 w-11/12 mx-auto text-sm">
+        <a href="https://www.flaticon.com/kr/free-icons/" title="철도 아이콘">
+          철도 아이콘 제작자: Design Circle - Flaticon
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
