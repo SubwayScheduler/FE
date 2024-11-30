ebf649b9cf46ff38c494099b85585088bab61cc5
kwdahun <kwdahun@naver.com>
2024-11-30 17:49:04 +0900

    Design: Footer content modified

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ const Footer = () => {
     <footer className="py-10 bg-black text-white">
       <div className="md:w-9/12 w-11/12 mx-auto text-sm pb-5">
         <div>사이트 제작자</div>
-        <div>동국대학교 컴퓨터공학과 권다훈</div>
-        <div>동국대학교 컴퓨터공학과 류성호</div>
+        <div>동국대학교 컴퓨터공학과 2021112062 권다훈</div>
+        <div>동국대학교 컴퓨터공학과 2020112021 류성호</div>
       </div>
       <div className="md:w-9/12 w-11/12 mx-auto text-sm">
         <a href="https://www.flaticon.com/kr/free-icons/" title="철도 아이콘">
